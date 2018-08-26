159c8e391604172d5bd689bc701071317162e078
Mario Domenech Goulart <mario@parenteses.org>
2018-08-26 11:38:15 +0200

    Add tests for spiffy-split-sexpr-log

--- tests/run.scm
+++ tests/run.scm
@@ -1,7 +1,8 @@
-(use test posix)
+(use test posix utils)
+
+(test-begin "spiffy-sexpr-log")
 
 (test-begin "spiffy-log2sexpr")
-
 (let ((sexpr-log
        (call-with-input-pipe
         (make-pathname ".." "spiffy-log2sexpr.scm access.log")
@@ -15,7 +16,24 @@
                       403
                       "-"
                       ("Wget/1.12 (linux-gnu)"))))))
-
 (test-end "spiffy-log2sexpr")
+
+(test-begin "spiffy-split-sexpr-log")
+(handle-exceptions exn
+  #f ;; FIXME: check exception type
+  (delete-directory "split" 'recursively))
+(system* "~a split"
+         (make-pathname ".." "spiffy-split-sexpr-log sexpr.log"))
+(test "split/2018/01/01.log"
+      (file-exists? (make-pathname (list "split" "2018" "01") "01.log")))
+(test "split/2018/02/01.log"
+      (file-exists? (make-pathname (list "split" "2018" "02") "01.log")))
+(test "split/2018/03/01.log"
+      (file-exists? (make-pathname (list "split" "2018" "03") "01.log")))
+(test "split/2018/04/01.log"
+      (file-exists? (make-pathname (list "split" "2018" "04") "01.log")))
+(test-end "spiffy-split-sexpr-log")
+
+(test-end "spiffy-sexpr-log")
 
 (test-exit)
